34fd059bb0675d548853b542cc84dae388673f42
Christopher Francisco <cjfg00gl3@gmail.com>
2024-12-06 13:01:35 -0600

    CREATE intro to getting started section notes

---
+++ lessons/00Introduction-Getting Started/0_1introductionToTheseTutorials.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Welcome, welcome, welcome, fluff, fluff, fluff." << std::endl;
+    std::cout << "Dark Reader is a good extension." << std::endl;
+    std::cout << "I'm using Brave so I'm not disabling the ad blocker." << std::endl;
+    return 0;
+}
