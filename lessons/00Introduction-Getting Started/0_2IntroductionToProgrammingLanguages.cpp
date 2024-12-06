d4b7eabc4f2cbbcffbb20a25775586fa45939c8b
Christopher Francisco <cjfg00gl3@gmail.com>
2024-12-06 14:03:18 -0600

    CREATE intro to programming languages notes

---
+++ lessons/00Introduction-Getting Started/0_2IntroductionToProgrammingLanguages.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(void)
+{
+    std::cout << "I know what a program is, wasted my time." << std::endl;
+    std::cout << "Different CPU architecture, different instruction sets. ARM is going to crush x86." << std::endl;
+    std::cout << "Yes, different architecture different CPU different assemblers, that should be pretty obvious. Wasted my time." << std::endl;
+    std::cout << "So compiled programs are safer because you don't have to redistribute the source code. Reverse engineering?" << std::endl;
+    std::cout << "I know what a high level program is, wasted by time." << std::endl;
+}
